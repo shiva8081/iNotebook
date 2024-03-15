34a27af6b6658e6e29d70e8d8106d6e9b4e44249
shiva8081 <yadavshiva8755@gmail.com>
2024-03-15 21:49:08 +0530

    update2

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
 //create a user using :  post'/api/auth'
-router.get('/',(req,res)=>{
-   console.log(req.body);
-   const user = User(req.body);
-   user.save()
-   res.send(res.body);
-  
+router.post('/', [
+    body('email').isEmail(),
+    body('name').isLength({ min: 5 }), 
+    body('password').isLength({ min: 5 })
+], (req, res) => {
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save()
+    // res.send(req.body);
+    if (result.isEmpty()) {
+        const data = matchedData(req);
+        return res.send(`Hello, ${data.person}!`);
+    }
+    // res.send(req.body)
+    res.send({ errors: result.array() });
+
 
 })
 module.exports = router 
