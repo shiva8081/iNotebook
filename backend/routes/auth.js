116a940a12088d1586e801f2a1770a4a1d297f57
shiva8081 <yadavshiva8755@gmail.com>
2024-03-16 01:25:19 +0530

    login_end_poiny

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,36 +2,75 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_secret="shivaloginadmin";
 
 
-//create a user using :  post'/api/auth'
+//create a user using :  post'/api/auth/create'
 router.post('/createuser', [
     body('email','enter a valid name').isEmail(),
     body('name','enter a valid email').isLength({ min: 5 }), 
     body('password','password must be longer then 5').isLength({ min: 5 })
 ], async (req, res) => {
- 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    // res.send(req.body)
-    res.send(req.body);
     let user =await  User.findOne({email:req.body.email})
     if(user){
         return res.status(400).json({error:'mail already exist'})
     }
-    user = await User.create({
+    const salt= await bcrypt.genSalt(10);
+    const secpass= await bcrypt.hash(req.body.password,salt);
+
+   const data= await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secpass,
         email: req.body.email, 
       })
-      
-    //   .then(user => res.json(user))
-    //   .catch(err=>{console.log(err)
-    // res.json({error:'please enter s unique email'})})
+    // console.log(data);
+     const data1={
+       id:data.id
+    }
+    const jswdata=jwt.sign(data1,JWT_secret)
+    console.log(jswdata)
+    // res.json()
 
-
+res.status(200).send({jswdata})
 })
+// authenticate a user using post"/api/auth/login
+router.post('/login', [
+    body('email','enter a valid name').isEmail(),
+    body('password','password cant be blank').exists()
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const{email,password}=req.body;
+    try {
+        let user= await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:'invalid attempt'})
+        }
+        const comparepass=await bcrypt.compare(password,user.password);
+        if(!comparepass){
+            return res.status(400).json({error:'invalid attempt'})
+        }
+        const data1={
+            id:user.id
+         }
+         const jswdata=jwt.sign(data1,JWT_secret)
+console.log("token generated:",jswdata);
+res.status(200).send({token:jswdata})
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+}
+)
+
+
 
 module.exports = router 
