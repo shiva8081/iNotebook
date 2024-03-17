257d9be22f9f78c09590ebeff9b2e4e3235eac2f
shiva8081 <yadavshiva8755@gmail.com>
2024-03-17 23:52:09 +0530

    backend

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,14 +1,14 @@
 var jwt = require('jsonwebtoken');
-const JWT_SECRET = 'Harryisagoodb$oy';
+const JWT_SECRET = 'shiva';
 
-const fetchuser = (req, res, next) => {
+const fetchuser = async (req, res, next) => {
     // Get the user from the jwt token and add id to req object
     const token = req.header('auth-token');
     if (!token) {
         res.status(401).send({ error: "Please authenticate using a valid token" })
     }
     try {
-        const data = jwt.verify(token, JWT_SECRET);
+        const data = await jwt.verify(token, JWT_SECRET);
         req.user = data.user;
         next();
     } catch (error) {
@@ -16,6 +16,6 @@ const fetchuser = (req, res, next) => {
     }
 
 }
-
+ 
 
 module.exports = fetchuser;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -17,7 +17,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 })
 
 
-// ROUTE 1: get all the notes: POST "/api/notes/addnotes". Login required
+// ROUTE 2: add notes: POST "/api/notes/addnotes". Login required
 
 router.post('/addnotes', fetchuser, [
     body('title', 'Enter a valid title').isLength({ min: 3 }),
@@ -32,7 +32,7 @@ router.post('/addnotes', fetchuser, [
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        const note = new note({
+        const note = new Notes({
             title, description, tag, user: req.user.id
         })
         const savedNote = await note.save();
@@ -43,4 +43,56 @@ router.post('/addnotes', fetchuser, [
 
 })
 
+
+// ROUTE 2: add notes: put "/api/notes/updatenote". Login required
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    const { title, description, tag } = req.body;
+    //create newnote object
+    const newNote = {};
+    if (title) { newNote.title = title }
+    if (description) { newNote.description = description }
+    if (tag) { newNote.tag = tag }
+
+
+    //find the note to be updated and update it
+
+    // const note=Notes.findByIdAndUpdate()
+
+    let note = await Notes.findById(req.params.id);
+    if (!note) { return res.status(400).send("not found") }
+    if (note.user.toString() != req.user.id) { return res.status(401).send("not allowed 1") }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+
+    res.json({ note });
+
+
+
+
+})
+
+
+// ROUTE 2: delete notes: delete "/api/notes/updatenote". Login required
+
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+
+    const { title, description, tag } = req.body;
+    
+
+
+    //find the note to be updated and update it
+
+    
+
+    let note = await Notes.findById(req.params.id);
+    if (!note) { return res.status(400).send("not found") }
+    if (note.user.toString() != req.user.id) { return res.status(401).send("not allowed 1") }
+
+    note = await Notes.findByIdAndDelete(req.params.id)
+
+    res.json({"success":"note has been deleted",note:note});
+
+})
 module.exports = router 
